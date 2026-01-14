a472614711cef170759e889366acf17a380bc1d7
Neil <neil.rabia972972@gmail.com>
2026-01-14 11:39:41 +0100

    Ajout de la vérification de la taille du fichier

--- app/Config/APIConfig.py
+++ app/Config/APIConfig.py
@@ -2,10 +2,11 @@ from app.Config import BaseConfig
 import os 
 # Configuration spécifique à l'API Flask
 class APIConfig(BaseConfig.BaseConfig):
-    API_PORT = int(os.getenv("API_PORT", "5000")) # Port d'écoute de l'API
-    HOST = "0.0.0.0" # Écoute sur toutes les interfaces réseau
+    API_PORT = int(os.getenv("API_PORT", "5000"))
+    HOST = "0.0.0.0"
 
-    # Formats audio autorisés
+    # Lié à l'upload des fichiers audio
     raw_formats = os.getenv("FORMAT_AUDIO_ALLOWED", "wav,mp3,ogg,m4a")
     FORMAT_AUDIO_ALLOWED = {f.strip() for f in raw_formats.split(",")}
+    MAX_AUDIO_SIZE_MB = int(os.getenv("MAX_AUDIO_SIZE_MB", "20"))
 
--- app/Middlewares/check_audio.py
+++ app/Middlewares/check_audio.py
@@ -1,28 +1,38 @@
 from flask import request
 from functools import wraps
 from app.Helpers.responses import error
+from app.Config.APIConfig import APIConfig
 
 """
 Décorateur qui vérifie l'authenticité d'un fichier audio : 
 - Existance 
 - Nom non vide
 - Mimetype du binaire
+- Taille maximale
 """
 
 def check_audio(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        if 'audioFile' not in request.files:
-            return error("Aucun fichier audio fourni", 400)
+        file = request.files.get('audioFile')
+        
+        # 1. Présence et Nom
+        if not file or file.filename == '':
+            return error("Fichier audio manquant ou invalide", 400)
 
-        audio_file = request.files['audioFile']
-
-        if audio_file.filename == '':
-            return error("Le fichier audio est vide", 400)
-
-        if not audio_file.mimetype.startswith("audio/"):
-            return error(f"Fichier non audio reçu (MIME={audio_file.mimetype})", 400)
+        # 2. Type de contenu
+        if not file.mimetype.startswith("audio/"):
+            return error(f"Format {file.mimetype} non supporté", 400)
+        
+        # 3. Validation Taille (encapsulée)
+        if not _is_size_valid(file, APIConfig.MAX_AUDIO_SIZE_MB):
+            return error(f"Fichier trop lourd (Max: {APIConfig.MAX_AUDIO_SIZE_MB}MB)", 400)
 
         return f(*args, **kwargs)
-
     return wrapper
+
+def _is_size_valid(file, max_mb):
+    file.seek(0, 2)
+    size = file.tell()
+    file.seek(0)
+    return size <= (max_mb * 1024 * 1024)
